3bfd67d6082544848f74c222ec9aff034d8ac9a3
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-19 10:42:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |name,data|
     data.each do |attribute,data|
-      :favorite_ice_cream_flavors[0].delete
+      :favorite_ice_cream_flavors["strawberry"].delete
 
       end
     end
